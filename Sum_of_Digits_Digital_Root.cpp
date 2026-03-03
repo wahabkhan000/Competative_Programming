012ffeb4566beb388be420cc9d7df603ffce5479
Abdul Wahab Akbar <itswahab00119@gmail.com>
2026-03-03 16:20:43 +0500

    Create Sum_of_Digits_Digital_Root.cpp

---
+++ Sum_of_Digits_Digital_Root.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int f=0;
+    if (n<10) {
+        cout<<n;
+    }
+    else {
+        while (n>0) {
+            f=0;
+            while (n>0) {
+                int rem=n%10;
+                n=n/10;
+                f=f+rem;
+            }
+            if (f<10) {
+                //if n==942;(9+4+2=15=1+5=6)
+                cout<<"The Digital root of"<<n<<"is"<<f<<endl;
+                return 0;
+            }
+            else {
+                n=f;
+            }
+        }
+    }
+}
