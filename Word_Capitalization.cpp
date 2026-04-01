671e0b4dc52d04a90820fad8446c5baba87a4730
Abdul Wahab Akbar <itswahab00119@gmail.com>
2026-04-01 18:02:11 +0500

    Add word capitalization logic in Word_Capitalization.cpp
    Implement word capitalization based on first character.

---
+++ Word_Capitalization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+    if (s[0] >='A' && s[0]<='Z') {
+        cout<<s<<endl;
+    }
+    else {
+        s[0]=s[0]-32;
+        cout<<s<<endl;
+    }
+}
