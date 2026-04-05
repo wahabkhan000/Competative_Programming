146729c4855d3efac72311c12094cda7e6aa1a72
Abdul Wahab Akbar <itswahab00119@gmail.com>
2026-04-05 20:23:15 +0500

    Add prime number finder in Ready_for_Prime_Time.cpp
    Implement a program to find and print prime numbers less than n.

---
+++ (Ready_for)_Prime_Time.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for (int i=2;i<n;i++) {
+        int count=0;
+        for (int j=1;j<=i;j++) {
+            if (i%j==0) {
+                count++;
+            }
+        }
+        if (count==2) {
+            cout<<i<<" ";
+        }
+    }
+}
